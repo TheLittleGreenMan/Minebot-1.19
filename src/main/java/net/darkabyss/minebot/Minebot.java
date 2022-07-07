e807056363551a85c6190708a3c11a74657b510a
BuildTools <unconfigured@null.spigotmc.org>
2022-07-06 22:20:20 -0500

    Set up Discord JDA API and started working on the initialization of Minebot.

--- src/main/java/net/darkabyss/minebot/Minebot.java
+++ src/main/java/net/darkabyss/minebot/Minebot.java
@@ -1,14 +1,24 @@
 package net.darkabyss.minebot;
 
+import net.dv8tion.jda.api.AccountType;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Minebot implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
 
+public class Minebot implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("minebot");
+	final String token = "Njc0MDExOTc3Mjc3ODk4NzY0.GzX4K5.m3lQdX-P6zX2VkNSf1CRQithdNXUUEvKdZlRAI";
+	public static final String chatid = "";
+	public static final String commandchatid = "";
+	//static JDA jda = null;
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		JDA jda = new JDABuilder.createDefault(token)
+							.setAutoReconnect(true)
+							.build().awaitReady();
 	}
+
 }
