6565cef9c6778d8e4ba184c6198c5cab4426c016
BuildTools <unconfigured@null.spigotmc.org>
2022-07-07 02:11:25 -0500

    Continuing to setup Discord API. Am having trouble having gradle work with both APIs.

--- src/main/java/net/darkabyss/minebot/Minebot.java
+++ src/main/java/net/darkabyss/minebot/Minebot.java
@@ -1,24 +1,40 @@
 package net.darkabyss.minebot;
 
-import net.dv8tion.jda.api.AccountType;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+//import net.dv8tion.jda.api.JDA;
+//import net.dv8tion.jda.api.JDABuilder;
+//import net.dv8tion.jda.api.entities.Activity;
+
+import javax.security.auth.login.LoginException;
 
 public class Minebot implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("minebot");
+	public static final String MOD_ID = "minebot";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	final String token = "Njc0MDExOTc3Mjc3ODk4NzY0.GzX4K5.m3lQdX-P6zX2VkNSf1CRQithdNXUUEvKdZlRAI";
 	public static final String chatid = "";
 	public static final String commandchatid = "";
-	//static JDA jda = null;
+	public Gson gson = new GsonBuilder().setPrettyPrinting().create();
+	//public static JDA jda = null;
 	@Override
 	public void onInitialize() {
-		JDA jda = new JDABuilder.createDefault(token)
-							.setAutoReconnect(true)
-							.build().awaitReady();
+	LOGGER.info("Hello Fabric World!");
+	/*		try {
+			 jda = JDABuilder.createDefault(token)
+								.setAutoReconnect(true)
+								.setActivity(Activity.playing("Minecraft"))
+								.build().awaitReady();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (LoginException e) {
+			e.printStackTrace();
+		}
+*/
 	}
 
 }
---
+++ src/main/java/net/darkabyss/minebot/MinebotClient.java
@@ -0,0 +1,10 @@
+package net.darkabyss.minebot;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinebotClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
